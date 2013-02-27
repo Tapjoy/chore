e6280e2911878de89d47a9b358fb00e9b2c345c7
Andy LeClair <andy.leclair@tapjoy.com>
2013-02-27 16:29:17 -0500

    s/lock/lease. adding comments because this is some complicated stuff. adding spex

--- lib/chore/semaphore.rb
+++ lib/chore/semaphore.rb
@@ -22,10 +22,10 @@ module Chore
 
     def acquire(&block)
       if block
-        wait_for_lock(&block)
+        wait_for_lease(&block)
       else
         if count < @max_leases
-          Lease.new(create_lock!, @zk)
+          Lease.new(create_lease!, @zk)
         else
           nil
         end
@@ -34,29 +34,32 @@ module Chore
 
     private
 
-    def wait_for_lock(&block)
+    def wait_for_lease(&block)
       # attempt to acquire a lock right now if we can
-      unless actually_acquire_lock(&block)
+      unless actually_acquire_lease(&block)
         begin
-          # set up the handler if we can't immediately get a lock
+          # set up the handler if we can't immediately get a lease
           @subscription = @zk.register(@resource_path) do |event|
-            actually_acquire_lock(&block)
+            actually_acquire_lease(&block)
           end
 
           # block until we acquire a lock
+          # see the rdoc for Queue (it blocks)
           @queue.pop
         ensure
-          @subscription.unsubscribe
+          @subscription.unsubscribe if @subscription
         end
       end
     end
 
-    def actually_acquire_lock(&block)
+    def actually_acquire_lease(&block)
       if count < @max_leases
+        # we need to unsubscribe before we create the lease
+        # otherwise we would fire the handler when we create it
         @subscription.unsubscribe if @subscription
         lease_path = nil
         begin
-          lease_path = create_lock!
+          lease_path = create_lease!
           yield block
         ensure
           @queue.enq(:acquired)
@@ -69,10 +72,13 @@ module Chore
     end
 
     def count
+      # gotta set that watch every time or our handler
+      # might not fire which would be such a bummer man
+      # yeah whoa
       @zk.children(@resource_path, watch: true).length
     end
 
-    def create_lock!
+    def create_lease!
       @zk.create("#{@resource_path}/", :mode => :ephemeral_sequential)
     end
 
--- spec/chore/semaphore_spec.rb
+++ spec/chore/semaphore_spec.rb
@@ -3,17 +3,61 @@ require 'spec_helper'
 describe Chore::Semaphore do
 
   let(:resource_name) { "sqs" }
-  let(:max_locks) { 1 }
-  let(:semaphore) { Chore::Semaphore.new(resource_name, max_locks: max_locks ) }
+  let(:max_leases) { 1 }
+  let(:semaphore) { Chore::Semaphore.new(resource_name, max_leases: max_leases ) }
   let(:zk) { double("zk") }
+  let(:count) { 0 }
 
-  before do
+  before(:each) do
     ZK.stub(:new) { zk }
     zk.stub(:mkdir_p)
+    semaphore.stub(:count) { count }
   end
 
   it "should have an acquire method" do
     semaphore.should respond_to :acquire
   end
 
+  describe "we must block until a lock is acquired" do
+    it "should run the block if a lease is available" do
+      semaphore.should_receive(:actually_acquire_lease) { true }
+      semaphore.acquire do
+        sleep(0.1)
+      end
+    end
+
+
+    describe "but there are no locks available" do
+      let(:count) { 1 }
+
+      it "should block if no leases are available" do
+        zk.should_receive(:register)
+        expect { 
+          Timeout::timeout(0.3) do
+            semaphore.acquire do
+              sleep(1)
+            end
+          end 
+        }.to raise_error(Timeout::Error)
+      end
+    end
+  end
+  
+  describe "we do not need to block on a lock" do
+
+    it "returns a lease when it can acquire a lock" do
+      semaphore.stub(:create_lease!) { "0" }
+      lease = semaphore.acquire
+      lease.should_not be_nil
+    end
+
+    describe "but there are no locks available" do
+      let(:count) { 1 }
+
+      it "returns nil" do
+        lease = semaphore.acquire
+        lease.should be_nil
+      end
+    end
+  end
 end
