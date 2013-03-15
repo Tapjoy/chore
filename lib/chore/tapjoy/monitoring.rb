a80620f6ae605d8d25acef78587fa86672bce586
Andy LeClair <andy.leclair@tapjoy.com>
2013-03-15 11:41:04 -0400

    fixing specs up, moving tapjoy specific code into a module to isolate it and make adding the handlers explicit

--- lib/chore/tapjoy/monitoring.rb
+++ lib/chore/tapjoy/monitoring.rb
@@ -1,17 +1,34 @@
 require 'watcher/client'
 require 'watcher/publisher/statsd'
 
-Watcher::Metric.publisher = Watcher::Publisher::Statsd.new
-Watcher::Metric.default_scope = "jobs"
+module Chore
+  module Tapjoy
+    def self.register_tapjoy_handlers!
+      Watcher::Metric.publisher = Watcher::Publisher::Statsd.new('localhost', '8127')
+      Watcher::Metric.default_scope = "jobs"
 
-%w{ finished failed timeout }.each do |state|
-  Chore.add_hook :"after_perform_#{state}" do
-    metric = Watcher::Metric.new("finished", attributes: { state: state })
-    metric.increment
+      after_message = Proc.new do |state|
+        metric = Watcher::Metric.new("finished", attributes: { state: state })
+        metric.increment
+      end
+
+      Chore.add_hook :on_failure do
+        after_message.call "failed"
+      end
+      Chore.add_hook :on_timeout do
+        after_message.call "timeout"
+      end
+      Chore.add_hook :on_rejected do 
+        after_message.call "rejected"
+      end
+      Chore.add_hook :after_perform do
+        after_message.call "completed"
+      end
+
+      Chore.add_hook :on_fetch_tell_monitoring do 
+        metric = Watcher::Metric.new("fetch")
+        metric.increment
+      end
+    end
   end
-end
-
-Chore.add_hook :on_fetch_tell_monitoring do 
-  metric = Watcher::Metric.new("fetch")
-  metric.increment
 end
--- spec/chore/worker_spec.rb
+++ spec/chore/worker_spec.rb
@@ -1,4 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'watcher/client'
+require 'chore/tapjoy/monitoring'
 
 class SimpleJob
   include Chore::Job
@@ -18,11 +20,30 @@ class TimeoutJob
   end
 end
 
+class BreakingJob
+  include Chore::Job
+  queue_options :name => 'test', :publisher => FakePublisher
+
+  def perform(*args)
+    raise "test"
+  end
+end
+
 describe Chore::Worker do
   let(:consumer) { double('consumer') }
   let(:job_args) { [1,2,'3'] }
   let(:job) { SimpleJob.job_hash(job_args) }
   let(:timeout_job) { TimeoutJob.job_hash(job_args) }
+  let(:breaking_job) { BreakingJob.job_hash(job_args) }
+  let(:metric) { double('metric') }
+
+  before do
+    metric.stub(:increment)
+  end
+
+  after do
+    Chore.clear_hooks!
+  end
 
   it 'should use a default encoder' do
     worker = Chore::Worker.new
@@ -30,7 +51,7 @@ describe Chore::Worker do
   end
 
   it 'should process a single job' do
-    work = Chore::UnitOfWork.new('1',Chore::JsonEncoder.encode(job), consumer)
+    work = Chore::UnitOfWork.new('1', Chore::JsonEncoder.encode(job), consumer)
     SimpleJob.should_receive(:perform).with(*job_args)
     consumer.should_receive(:complete).with('1')
     w = Chore::Worker.new(work)
@@ -52,5 +73,18 @@ describe Chore::Worker do
     consumer.should_not_receive(:complete)
     Chore.should_receive(:run_hooks_for).with(:on_timeout, anything())
     Chore::Worker.start(work)
+  end
+
+  it 'should process after message hooks with success or failure' do
+    Chore::Tapjoy::register_tapjoy_handlers!
+    work = []
+    work << Chore::UnitOfWork.new('1', Chore::JsonEncoder.encode(job), consumer)
+    work << Chore::UnitOfWork.new('1', Chore::JsonEncoder.encode(breaking_job), consumer) 
+    consumer.should_receive(:complete).with('1')
+    # we need this twice because they're different instances?
+    Watcher::Metric.should_receive(:new).with("finished", attributes: { state: "completed" }) { metric }
+    Watcher::Metric.should_receive(:new).with("finished", attributes: { state: "failed" }) { metric }
+    w = Chore::Worker.new(work)
+    w.start
   end
 end
