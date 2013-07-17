f83210187cc978858852aea32476c8f7176a281e
StabbyCutyou <smattkelly@gmail.com>
2013-07-17 17:27:53 -0400

    updating specs
    removing dead code
    
    Whatever, PFEIFER. Fixing incorrect stub

--- spec/chore/queues/sqs/consumer_spec.rb
+++ spec/chore/queues/sqs/consumer_spec.rb
@@ -7,12 +7,14 @@ describe Chore::Queues::SQS::Consumer do
   let(:options) { {} }
   let(:consumer) { Chore::Queues::SQS::Consumer.new(queue_name) }
   let(:message) { TestMessage.new("handle","message body") }
+  let(:pool) { double("pool") }
 
   before do
     AWS::SQS.any_instance.stub(:queues).and_return { queues }
     queues.stub(:named) { queue }
     queue.stub(:receive_message) { message }
     queue.stub(:visibility_timeout) { 10 }
+    pool.stub(:empty!) { nil }
   end
 
   describe "consuming messages" do
@@ -42,14 +44,15 @@ describe Chore::Queues::SQS::Consumer do
 
   describe '#reset_connection!' do
     it 'should reset the connection after a call to reset_connection!' do
-      sqs = consumer.send(:sqs)
+      AWS::Core::Http::ConnectionPool.stub(:pools).and_return([pool])
+      pool.should_receive(:empty!)
       Chore::Queues::SQS::Consumer.reset_connection!
-      sqs.should_not be consumer.send(:sqs)
+      consumer.send(:queue)
     end
 
     it 'should not reset the connection between calls' do
-      sqs = consumer.send(:sqs)
-      sqs.should be consumer.send(:sqs)
+      sqs = consumer.send(:queue)
+      sqs.should be consumer.send(:queue)
     end
   end
 end
