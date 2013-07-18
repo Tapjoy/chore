076c8d6a416d45ea0c78163c2fd81d3403a4a2eb
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2013-07-18 15:05:23 -0400

    Fix SQS queues getting created for every message that gets published. ARCH-3 #close

--- lib/chore/queues/sqs/publisher.rb
+++ lib/chore/queues/sqs/publisher.rb
@@ -10,15 +10,16 @@ module Chore
           @sqs = AWS::SQS.new(
               :access_key_id => Chore.config.aws_access_key,
               :secret_access_key => Chore.config.aws_secret_key)
+          @sqs_queues = {}
         end
 
         def publish(queue_name,job)
-          queue = ensure_queue! queue_name
+          queue = self.queue(queue_name)
           queue.send_message(encode_job(job))
         end
 
-        def ensure_queue!(name)
-          @sqs.queues.create(name)
+        def queue(name)
+          @sqs_queues[name] ||= @sqs.queues.named(name)
         end
       end
     end
--- spec/chore/queues/sqs/publisher_spec.rb
+++ spec/chore/queues/sqs/publisher_spec.rb
@@ -4,17 +4,36 @@ module Chore
   describe Queues::SQS::Publisher do
     let(:job) { {'class' => 'TestJob', 'args'=>[1,2,'3']}}
     let(:queue_name) { 'test_queue' }
-    let(:queue) { double('queue') }
+    let(:queue) { double('queue', :send_message => nil) }
+    let(:sqs) do
+      double('sqs', :queues => double('queues', :named => queue))
+    end
     let(:publisher) { Queues::SQS::Publisher.new }
 
     before(:each) do
-      AWS::SQS.stub(:new)
-      publisher.should_receive(:ensure_queue!).with(queue_name).and_return(queue)
+      AWS::SQS.stub(:new).and_return(sqs)
     end
 
     it 'should create send an encoded message to the specified queue' do
       queue.should_receive(:send_message).with(job.to_json)
       publisher.publish(queue_name,job)
+    end
+
+    it 'should lookup the queue when publishing' do
+      sqs.queues.should_receive(:named).with('test_queue')
+      publisher.publish('test_queue', job)
+    end
+
+    it 'should lookup multiple queues if specified' do
+      sqs.queues.should_receive(:named).with('test_queue')
+      sqs.queues.should_receive(:named).with('test_queue2')
+      publisher.publish('test_queue', job)
+      publisher.publish('test_queue2', job)
+    end
+
+    it 'should only lookup a named queue once' do
+      sqs.queues.should_receive(:named).with('test_queue').once
+      2.times { publisher.publish('test_queue', job) }
     end
   end
 end
