2396be2379782f278836ec7e3c4287cdfb13c43e
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2013-08-22 18:15:57 -0400

    Reset the Rails logger after being forked

--- lib/chore/railtie.rb
+++ lib/chore/railtie.rb
@@ -3,5 +3,15 @@ module Chore
     rake_tasks do
       Dir[File.join(File.dirname(__FILE__),'tasks/*.task')].each { |f| load f }
     end
+
+    config.after_initialize do
+      if Chore.configuring?
+        # Reset the logger on forks to avoid deadlocks
+        Rails.logger = Chore.logger
+        Chore.add_hook(:after_fork) do |worker|
+          Rails.logger = Chore.logger
+        end
+      end
+    end
   end
 end
