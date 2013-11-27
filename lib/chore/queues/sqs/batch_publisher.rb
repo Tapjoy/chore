fe177f16fce565af9569c86021db56b7bde4314b
deanxorr <dean.dieker@tapjoy.com>
2013-11-26 19:32:54 -0600

    added batch publisher

---
+++ lib/chore/queues/sqs/batch_publisher.rb
@@ -0,0 +1,70 @@
+require 'chore/publisher'
+
+module Chore
+  module Queues
+    module SQS
+      class BatchPublisher < Chore::SQS::Publisher
+        @@reset_next = true
+
+        def initialize(opts={})
+          super
+          @queue = []
+          @timer = nil
+          @running = false
+          @interval = opts[:interval] || 10
+        end
+
+        def start(queue_name, interval)
+          @running = true
+          queue = self.queue(queue_name)
+          @timer = Thread.new do
+            loop do
+              sleep(interval)
+              empty_queue
+            end
+          end
+        end
+
+        def publish(job)
+          @queue << {:message_body => encode_job(job)}
+        end
+
+        def empty_queue
+          sqs.batch_send(@queue.slice!(0..9))
+        end
+
+        def self.reset_connection!
+          @@reset_next = true
+        end
+
+        def sqs
+          @sqs ||= AWS::SQS.new(
+            :access_key_id => Chore.config.aws_access_key,
+            :secret_access_key => Chore.config.aws_secret_key,
+            :logger => Chore.logger,
+            :log_level => :debug)
+        end
+
+        def publish(queue_name,job)
+          start(queue_name, @interval) unless @running
+          queue = self.queue(queue_name)
+          queue.send_message(encode_job(job))
+        end
+
+        def queue(name)
+         if @@reset_next
+            AWS::Core::Http::ConnectionPool.pools.each do |p|
+              p.empty!
+            end
+            @sqs = nil
+            @@reset_next = false
+            @sqs_queues = {}
+            @sqs_queue_urls = {}
+          end
+          @sqs_queue_urls[name] ||= self.sqs.queues.url_for(name)
+          @sqs_queues[name] ||= self.sqs.queues[@sqs_queue_urls[name]]
+        end
+      end
+    end
+  end
+end
