ec585139d6a81875cd7415173efb4e5fa678cc57
Lionel Honda <lionel.honda@tapjoy.com>
2016-07-14 16:27:12 -0700

    minor fixes

--- lib/chore/strategies/worker/helpers/worker_info.rb
+++ lib/chore/strategies/worker/helpers/worker_info.rb
@@ -4,9 +4,9 @@ module Chore
       # Holds meta information about the worker: pid, and connection socket
       attr_accessor :pid, :socket
 
-      def initialize(socket)
-        @pid = nil
-        @socket = socket
+      def initialize(pid)
+        @pid = pid
+        @socket = nil
       end
     end
   end
--- spec/chore/strategies/worker/helpers/worker_info_spec.rb
+++ spec/chore/strategies/worker/helpers/worker_info_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe Chore::Strategy::WorkerInfo do
-  let(:socket)      { double('socket') }
+  let(:pid)         { 1 }
   let(:worker_info) { Chore::Strategy::WorkerInfo.new(pid) }
 
   context '#initialize' do
-    it 'should initialize the WorkerInfo with a socket' do
-      wi = Chore::Strategy::WorkerInfo.new(socket)
-      expect(wi.socket).to equal(socket)
-      expect(wi.pid).to equal(nil)
+    it 'should initialize the WorkerInfo with a pid' do
+      wi = Chore::Strategy::WorkerInfo.new(pid)
+      expect(wi.pid).to equal(pid)
+      expect(wi.socket).to equal(nil)
     end
   end
 end
