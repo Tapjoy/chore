d83a867564e48aea1da33dfd800795846e7236b6
James Hong <ojameso@hotmail.com>
2025-09-08 16:43:48 -0700

    [NPE-691] Clean up redundant spec

--- spec/chore/queues/pubsub_spec.rb
+++ spec/chore/queues/pubsub_spec.rb
@@ -3,53 +3,44 @@ require 'spec_helper'
 describe Chore::Queues::PubSub do
   include_context 'fake pubsub objects'
 
+  let(:topic_admin) { double('topic_admin') }
+  let(:subscription_admin) { double('subscription_admin') }
+  let(:topic_path) { "projects/test/topics/#{queue_name}" }
+  let(:subscription_path) { "projects/test/subscriptions/#{subscription_name}" }
+
   before(:each) do
     allow(Chore::Queues::PubSub).to receive(:pubsub_client).and_return(pubsub_client)
     allow(Chore).to receive(:prefixed_queue_names).and_return([queue_name])
   end
 
   context "when managing queues" do
-    it 'should create topics and subscriptions that are defined in its internal job name list' do
-      topic_admin = double('topic_admin')
-      subscription_admin = double('subscription_admin')
+    before(:each) do
       allow(pubsub_client).to receive(:topic_admin).and_return(topic_admin)
       allow(pubsub_client).to receive(:subscription_admin).and_return(subscription_admin)
-      allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return("projects/test/topics/#{queue_name}")
-      allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return("projects/test/subscriptions/#{subscription_name}")
-      expect(topic_admin).to receive(:create_topic).with(name: "projects/test/topics/#{queue_name}").and_return(topic)
+      allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return(topic_path)
+      allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return(subscription_path)
+    end
+    it 'should create topics and subscriptions that are defined in its internal job name list' do
+      expect(topic_admin).to receive(:create_topic).with(name: topic_path).and_return(topic)
       expect(subscription_admin).to receive(:create_subscription).with(
-        name: "projects/test/subscriptions/#{subscription_name}",
-        topic: "projects/test/topics/#{queue_name}"
+        name: subscription_path,
+        topic: topic_path
       )
       Chore::Queues::PubSub.create_queues!
     end
 
     it 'should delete topics and subscriptions that are defined in its internal job name list' do
-      topic_admin = double('topic_admin')
-      subscription_admin = double('subscription_admin')
-      allow(pubsub_client).to receive(:topic_admin).and_return(topic_admin)
-      allow(pubsub_client).to receive(:subscription_admin).and_return(subscription_admin)
-      allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return("projects/test/topics/#{queue_name}")
-      allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return("projects/test/subscriptions/#{subscription_name}")
-      
       expect(Chore.logger).to receive(:info).with("Chore Deleting Pub/Sub Topic and Subscription: #{queue_name}")
-      expect(subscription_admin).to receive(:delete_subscription).with(subscription: "projects/test/subscriptions/#{subscription_name}")
-      expect(topic_admin).to receive(:delete_topic).with(topic: "projects/test/topics/#{queue_name}")
+      expect(subscription_admin).to receive(:delete_subscription).with(subscription: subscription_path)
+      expect(topic_admin).to receive(:delete_topic).with(topic: topic_path)
       
       Chore::Queues::PubSub.delete_queues!
     end
 
     context 'when handling delete errors' do
       it 'should continue deleting topic even if subscription delete fails' do
-        topic_admin = double('topic_admin')
-        subscription_admin = double('subscription_admin')
-        allow(pubsub_client).to receive(:topic_admin).and_return(topic_admin)
-        allow(pubsub_client).to receive(:subscription_admin).and_return(subscription_admin)
-        allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return("projects/test/topics/#{queue_name}")
-        allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return("projects/test/subscriptions/#{subscription_name}")
-        
         allow(subscription_admin).to receive(:delete_subscription).and_raise(Google::Cloud::NotFoundError.new('Subscription not found'))
-        expect(topic_admin).to receive(:delete_topic).with(topic: "projects/test/topics/#{queue_name}")
+        expect(topic_admin).to receive(:delete_topic).with(topic: topic_path)
         expect(Chore.logger).to receive(:info).with("Chore Deleting Pub/Sub Topic and Subscription: #{queue_name}")
         expect(Chore.logger).to receive(:error).with("Deleting Subscription: #{queue_name} failed because Subscription not found")
         
@@ -57,14 +48,7 @@ describe Chore::Queues::PubSub do
       end
 
       it 'should continue even if topic delete fails' do
-        topic_admin = double('topic_admin')
-        subscription_admin = double('subscription_admin')
-        allow(pubsub_client).to receive(:topic_admin).and_return(topic_admin)
-        allow(pubsub_client).to receive(:subscription_admin).and_return(subscription_admin)
-        allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return("projects/test/topics/#{queue_name}")
-        allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return("projects/test/subscriptions/#{subscription_name}")
-        
-        expect(subscription_admin).to receive(:delete_subscription).with(subscription: "projects/test/subscriptions/#{subscription_name}")
+        expect(subscription_admin).to receive(:delete_subscription).with(subscription: subscription_path)
         allow(topic_admin).to receive(:delete_topic).and_raise(Google::Cloud::NotFoundError.new('Topic not found'))
         expect(Chore.logger).to receive(:info).with("Chore Deleting Pub/Sub Topic and Subscription: #{queue_name}")
         expect(Chore.logger).to receive(:error).with("Deleting Topic: #{queue_name} failed because Topic not found")
@@ -75,9 +59,12 @@ describe Chore::Queues::PubSub do
 
     context 'and checking for existing queues' do
       it 'checks for existing topics/subscriptions' do
-        allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return("projects/test/topics/#{queue_name}")
-        allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return("projects/test/subscriptions/#{subscription_name}")
+        allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return(topic_path)
+        allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return(subscription_path)
         expect(described_class).to receive(:existing_queues).and_return([])
+        # Since existing_queues returns [], create_queues! will proceed to create topics/subscriptions
+        allow(topic_admin).to receive(:create_topic).and_return(topic)
+        allow(subscription_admin).to receive(:create_subscription)
         Chore::Queues::PubSub.create_queues!(true)
       end
 
@@ -87,12 +74,6 @@ describe Chore::Queues::PubSub do
       end
 
       it 'does not raise an error if a topic/subscription does not exist' do
-        topic_admin = double('topic_admin')
-        subscription_admin = double('subscription_admin')
-        allow(pubsub_client).to receive(:topic_admin).and_return(topic_admin)
-        allow(pubsub_client).to receive(:subscription_admin).and_return(subscription_admin)
-        allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return("projects/test/topics/#{queue_name}")
-        allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return("projects/test/subscriptions/#{subscription_name}")
         allow(topic_admin).to receive(:create_topic).and_return(topic)
         allow(subscription_admin).to receive(:create_subscription)
         allow(described_class).to receive(:existing_queues).and_return([])
@@ -102,12 +83,6 @@ describe Chore::Queues::PubSub do
 
     context 'when handling already exists errors' do
       it 'should continue when topic already exists' do
-        topic_admin = double('topic_admin')
-        subscription_admin = double('subscription_admin')
-        allow(pubsub_client).to receive(:topic_admin).and_return(topic_admin)
-        allow(pubsub_client).to receive(:subscription_admin).and_return(subscription_admin)
-        allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return("projects/test/topics/#{queue_name}")
-        allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return("projects/test/subscriptions/#{subscription_name}")
         allow(topic_admin).to receive(:create_topic).and_raise(Google::Cloud::AlreadyExistsError.new('Topic already exists'))
         allow(subscription_admin).to receive(:create_subscription).and_return(subscription)
         expect(Chore.logger).to receive(:info).with("Chore Creating Pub/Sub Topic and Subscription: #{queue_name}")
@@ -116,12 +91,6 @@ describe Chore::Queues::PubSub do
       end
 
       it 'should continue when subscription already exists' do
-        topic_admin = double('topic_admin')
-        subscription_admin = double('subscription_admin')
-        allow(pubsub_client).to receive(:topic_admin).and_return(topic_admin)
-        allow(pubsub_client).to receive(:subscription_admin).and_return(subscription_admin)
-        allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return("projects/test/topics/#{queue_name}")
-        allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return("projects/test/subscriptions/#{subscription_name}")
         allow(topic_admin).to receive(:create_topic).and_return(topic)
         allow(subscription_admin).to receive(:create_subscription).and_raise(Google::Cloud::AlreadyExistsError.new('Subscription already exists'))
         expect(Chore.logger).to receive(:info).with("Chore Creating Pub/Sub Topic and Subscription: #{queue_name}")
@@ -130,12 +99,6 @@ describe Chore::Queues::PubSub do
       end
 
       it 'should continue when both topic and subscription already exist' do
-        topic_admin = double('topic_admin')
-        subscription_admin = double('subscription_admin')
-        allow(pubsub_client).to receive(:topic_admin).and_return(topic_admin)
-        allow(pubsub_client).to receive(:subscription_admin).and_return(subscription_admin)
-        allow(pubsub_client).to receive(:topic_path).with(queue_name).and_return("projects/test/topics/#{queue_name}")
-        allow(pubsub_client).to receive(:subscription_path).with(subscription_name).and_return("projects/test/subscriptions/#{subscription_name}")
         allow(topic_admin).to receive(:create_topic).and_raise(Google::Cloud::AlreadyExistsError.new('Topic already exists'))
         allow(subscription_admin).to receive(:create_subscription).and_raise(Google::Cloud::AlreadyExistsError.new('Subscription already exists'))
         expect(Chore.logger).to receive(:info).with("Chore Creating Pub/Sub Topic and Subscription: #{queue_name}")
